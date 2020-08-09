4cdd7f5cba23ce33178ed377bdd206ba24bd190b
Griffin Cosgrove <griffcosgrove@gmail.com>
2020-08-09 14:06:35 -0400

    added complete inv

---
+++ column.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import json, csv
+from openpyxl import load_workbook
+
+
+#with open('inv.csv', 'rt') as csvdata:
+#    next(csvdata, None) # skip the headers
+#    reader = csv.DictReader(csvdata,fieldnames=['Column', 'Bottle from top (Number)','Year, Vintage, or NV', 'Name', 'Type'])
+#    json.dump([row for row in reader], open('output.json', 'w+'))
